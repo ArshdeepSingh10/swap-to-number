b1a33d5429ff918932b0c90806726eff0ab688c3
ArshdeepSingh10 <114170992+ArshdeepSingh10@users.noreply.github.com>
2022-09-22 21:50:56 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main{
+	public static void main(String a[]) {
+		int b = 2;
+		int c = 3;
+		System.out.println("Before swaping the number : b = "+ b +", c = "+ c);
+		int temp = b ;
+		b = c;
+		c = temp;
+		System.out.println("After swaping the number : b = "+ b +", c = "+ c);
+		
+		
+	}
+}
